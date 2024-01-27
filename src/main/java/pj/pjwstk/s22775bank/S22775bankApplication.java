2292735134d8c736b7c7a8c37bd819e3af364b64
s22775-pj-Oleksii-Sumrii <74150190+s22775-pj-Oleksii-Sumrii@users.noreply.github.com>
2024-01-27 17:26:53 +0100

    https://github.com/s22775-pj-Oleksii-Sumrii/s22775bank.git

---
+++ src/main/java/pj/pjwstk/s22775bank/S22775bankApplication.java
@@ -0,0 +1,13 @@
+package pj.pjwstk.s22775bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S22775bankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S22775bankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pj/pjwstk/s22775bank/S22775bankApplicationTests.java
@@ -0,0 +1,13 @@
+package pj.pjwstk.s22775bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S22775bankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
